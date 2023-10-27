f1d98ff469aee5bc7882445679c233e64d17e6d3
Stamboliysky <jordanspassoff@gmail.com>
2023-10-27 18:39:43 +0200

    Añadir ejercico-07

--- src/ejercicio-07/README.md
+++ src/ejercicio-07/README.md
@@ -7,5 +7,6 @@ function calcularAreaRectangulo(base, altura) {
   return base * altura
 }
 
+
 console.log(calcularAreaRectangulo(2, 4))
 ```
--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Ejercicio 07 Parametros y Argumentos:
+
+function calcularAreaRectangulo(base, altura) {
+    return base * altura 
+    
+}
+console.log (calcularAreaRectangulo(2,4))
