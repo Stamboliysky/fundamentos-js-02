fab16fd45e8460de6f99850f63504926322ffb63
Stamboliysky <jordanspassoff@gmail.com>
2023-10-28 09:51:53 +0200

    Añadir ejercicio-13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+// Ejercicio 13: Closures
+
+function contadorClosure() {
+    let contador = 0
+
+    return function () {
+        contador++
+        return contador 
+    }
+}
+const incrementar = contadorClosure()
+console.log(incrementar())
+console.log(incrementar())
